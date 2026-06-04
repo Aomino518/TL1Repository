94e0e4f782588e28eee4dd9967e113c313477902
Aoto Mori <k024g0051@g.neec.ac.jp>
2026-06-04 23:42:02 +0900

    01_06完了
    親オブジェクトの名前とTranformを表示実装

--- level_editor.py
+++ level_editor.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 #blenderに登録するアドオン情報
 bl_info = {
@@ -52,6 +53,7 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         #context : カーソルに合わせた時のポップアップのカスタマイズなどに使用
     
         #トップバーの「エディターメニュー」に項目(オペレータ)を追加
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname, text = MYADDON_OT_export_scene.bl_label)
         self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text = MYADDON_OT_stretch_vertex.bl_label)
         self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text = MYADDON_OT_create_ico_sphere.bl_label)
         self.layout.operator("wm.url_open_preset", text = "Manual", icon = 'HELP')
@@ -92,8 +94,45 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
 
         return {'FINISHED'}
 
+# オペレータ シーンの出力
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーン情報をExportします"
+
+    def execute(self, context):
+
+        print("シーン情報をexportします")
+
+        # シーン内の全オブジェクトについて
+        for object in bpy.context.scene.objects:
+            print(object.type + " - " + object.name)
+            # ローカルトランフォーム行列から平行移動、回転、スケーリングを抽出
+            # 型は VectorQuaternion, Vector
+            trans, rot, scale = object.matrix_local.decompose()
+            # 回転を Quaternion から Euler (3軸での回転角) に変換
+            rot = rot.to_euler()
+            # ラジアンから度数法に変換
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)        
+            # トランスフォームの情報を表示
+            print("Trans(%f, %f, %f)" % (trans.x, trans.y, trans.z))
+            print("Rot(%f, %f, %f)" % (rot.x, rot.y, rot.z))
+            print("Scale(%f, %f, %f)" % (scale.x, scale.y, scale.z))
+            # 親オブジェクトの名前を表示
+            if object.parent:
+                print("Parent:" + object.parent.name)
+            print()
+            
+        print("シーン情報をExportしました")
+        self.report({'INFO'}, "シーン情報をExportしました")
+
+        return {'FINISHED'}
+
 #Blenderに登録するクラスリスト
 classes = (
+    MYADDON_OT_export_scene,
     MYADDON_OT_create_ico_sphere,
     MYADDON_OT_stretch_vertex,
     TOPBAR_MT_my_menu,
